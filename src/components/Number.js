bba5bcaa43bd7eb2e7681ed3eb743970136d0cad
Msdalmajdi <msdalmajdi@hotmail.com>
2022-05-14 21:04:40 +0300

    lots of progress

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,14 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
 function Number({ money }) {
-  const [numberClass, setNumberClass] = useState("money-amount-trans");
-  useEffect(() => {
-    const id = setInterval(() => console.log("haha"), 99999);
-
-    return () => {
-      clearInterval(id);
-    };
-  }, []);
   return (
     <span id="haha" className={numberClass}>
       {money}
